2bf738f4ccab5ffaa06b66f6e8c28fdfa9f08217
salaaad2 <arthurdurant263@gmail.com>
2020-01-15 15:07:04 +0100

    cool README

---
+++ README.md
@@ -0,0 +1,9 @@
+# ft_kernel_panic
+
+Kernel Panic is a cool function to stress test OSs, the function is a simple way to allocate memory until the OS decides to kill the program.
+This function has been tested on void linux, which simply stops it after some time.
+
+
+## How do I test it ?
+
+simply copy **kernel_panic.c** somewhere, compile it, then run it, simple.
--- kernel_panic.c
+++ kernel_panic.c
@@ -1,12 +1,30 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   kernel_panic.c                                   .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: fmoenne- <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2020/01/15 15:00:17 by fmoenne-     #+#   ##    ##    #+#       */
+/*   Updated: 2020/01/15 15:01:05 by fmoenne-    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
-int		main(void) 
+void	ft_kernel_panic(void)
 {
 	int *p;
+
 	while(1)
 	{
 		int inc = 1024 * 1024 * sizeof(char);
 		p = (int*)calloc(1,inc);
 	}
+}
+
+int		main(void) 
+{
+	ft_kernel_panic();
 }
